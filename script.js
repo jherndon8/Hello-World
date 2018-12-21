cff3d3a4416e0adf45154122c21a8d7b1fc0094a
jherndon8 <jherndon8@gatech.edu>
2018-12-21 01:36:22 -0500

    Added pokemon test

--- script.js
+++ script.js
@@ -6,4 +6,7 @@
     $("#mytext").fontSize = fontsize;
     $("mytext").append("a");
   });
+  fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+  .then(response => response.json())
+  .then(json => console.log(json))
 });
